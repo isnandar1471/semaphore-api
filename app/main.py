5fd67b4573680d836b170d3f220eee0ab91706ba
isnandar1471 <isnandar.1471@gmail.com>
2023-06-06 13:47:09 +0700

    tes

--- app/main.py
+++ app/main.py
@@ -84,7 +84,7 @@ def predict_image(request: fastapi.Request, file: fastapi.UploadFile | None = No
     }
 
 
-app.mount("/asset/upload", fastapi.staticfile.StaticFiles(directory="asset/upload"))
+app.mount("/asset/upload", fastapi.staticfiles.StaticFiles(directory="asset/upload"))
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)

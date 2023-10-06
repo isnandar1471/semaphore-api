20b4004bff9620fdd4f7c54debdd875b15eb8fb1
isnandar1471 <isnandar.1471@gmail.com>
2023-10-06 14:23:29 +0700

    update check-server-status.py

--- console/check-server-status.py
+++ console/check-server-status.py
@@ -5,19 +5,14 @@ import os
 import dotenv
 
 
-dotenv_filepath = dotenv.find_dotenv()
-
-if dotenv_filepath == "":
-    print(".env file not found. using default value")
+dotenv_filepath = dotenv.find_dotenv(raise_error_if_not_found=True)
 
 dotenv.load_dotenv(dotenv_path=dotenv_filepath)
 
-is_success = False
-
 try:
-    requests.get(f'http://127.0.0.1:{os.getenv("APP_SERVER_PORT", "8888")}/')
+    requests.get(f"http://127.0.0.1:{os.getenv('APP_SERVER_PORT', '8888')}/")
     print("server on ready")
 except Exception as e:
-    requests.get(f"https://api.telegram.org/bot6405514280:AAGs3xpeBAtoqAQqk-_-sUQqXGdhJ9nMOhY/sendMessage?chat_id=1150088790&text=servererror {e}")
-    print("server error")
-    print(e)
+    error_string = f"servererror {e}"
+    requests.get(f"{os.getenv('APP_TELEGRAMBOT_URL')}{error_string}")
+    print(error_string)

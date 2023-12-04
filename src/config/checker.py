5ce5c33b06d2064af8f18b31389a548769251fa6
isnandar1471 <isnandar.1471@gmail.com>
2023-12-04 16:06:06 +0700

    update

--- src/config/checker.py
+++ src/config/checker.py
@@ -7,10 +7,10 @@ def check_directories():
     directories = src.config.constant.CLASSES
 
     for directory in directories:
-        dir_path = os.path.join(base_dir, directory)
+        dir_path = os.path.join(base_dir, directory.lower())
         if not os.path.isdir(dir_path):
-            raise ValueError(f"Directory '{directory}' does not exist.")
+            raise ValueError(f"Directory '{dir_path}' does not exist.")
         else:
             files = os.listdir(dir_path)
             if len(files) == 0:
-                raise ValueError(f"Directory '{directory}' is empty.")
+                raise ValueError(f"Directory '{dir_path}' is empty.")
